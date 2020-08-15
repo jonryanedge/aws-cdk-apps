a93a4a51189352224256765e029f90d874988060
Jon Edgeworth <edgeworthjr@awsedge>
2020-08-14 21:51:44 -0400

    Updated environment

--- bastion/bin/bastion.ts
+++ bastion/bin/bastion.ts
@@ -3,5 +3,12 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { BastionStack } from '../lib/bastion-stack';
 
+const env = {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+};
+
 const app = new cdk.App();
-new BastionStack(app, 'BastionStack');
+new BastionStack(app, 'BastionStack', {
+    env: env,
+});
