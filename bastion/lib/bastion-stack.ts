9c8a2c9013ca0c0c2bab72cac600d0006b43b37b
Jon Edgeworth <edgeworthjr@awsedge>
2020-08-14 21:13:52 -0400

    Upgraded CDK version

--- bastion/lib/bastion-stack.ts
+++ bastion/lib/bastion-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class BastionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "vpc", {
+      
+    });
   }
 }
