c9c9f3eca109e130294d021dbf4a2c517b19ab05
jonryanedge <jredgeworth@gmail.com>
2020-08-21 20:42:37 -0400

    Updated VPC subnets

--- bastion/lib/bastion-stack.ts
+++ bastion/lib/bastion-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import { SubnetType } from '@aws-cdk/aws-ec2';
 
 export class BastionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,7 +8,22 @@ export class BastionStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const vpc = new ec2.Vpc(this, "vpc", {
-      
+      cidr: '10.255.0.0/20',
+      subnetConfiguration: [{
+        cidrMask: 26,
+        name: 'publicNet',
+        subnetType: SubnetType.PUBLIC,
+      },
+      {
+        cidrMask: 26,
+        name: 'isoNet',
+        subnetType: SubnetType.ISOLATED,
+      },
+      {
+        cidrMask: 26,
+        name: 'dmzNet',
+        subnetType: SubnetType.PRIVATE,
+      }]
     });
   }
 }
