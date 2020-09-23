9641780e4ff26ac4bb22216d12a565721bd89700
jonryanedge <edgeworthjr@gmail.com>
2020-09-23 08:24:06 -0400

    Updated bastion vpc settings

--- bastion/lib/bastion-stack.ts
+++ bastion/lib/bastion-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import { SubnetType, FlowLogDestination } from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class BastionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,22 +12,42 @@ export class BastionStack extends cdk.Stack {
       subnetConfiguration: [{
         cidrMask: 26,
         name: 'publicNet',
-        subnetType: SubnetType.PUBLIC,
+        subnetType: ec2.SubnetType.PUBLIC,
       },
       {
         cidrMask: 26,
         name: 'isoNet',
-        subnetType: SubnetType.ISOLATED,
+        subnetType: ec2.SubnetType.ISOLATED,
       },
       {
         cidrMask: 26,
         name: 'dmzNet',
-        subnetType: SubnetType.PRIVATE,
+        subnetType: ec2.SubnetType.PRIVATE,
       }],
+      natGateways: 1,
       flowLogs: {
         VpcFlowLogs: {
-          destination: FlowLogDestination.toCloudWatchLogs(),
+          destination: ec2.FlowLogDestination.toCloudWatchLogs(),
         }
+      }
+    });
+
+    const amzn_linux = ec2.MachineImage.latestAmazonLinux({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+    });
+
+    const role = new iam.Role(this, 'InstanceSSM', {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+      managedPolicies: [ iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonEC2RoleforSSM") ],
+    });
+
+    const bastion = new ec2.Instance(this, 'bastion', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2,ec2.InstanceSize.NANO),
+      machineImage: amzn_linux,
+      role: role,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE,
       }
     });
   }
